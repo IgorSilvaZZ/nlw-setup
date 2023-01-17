053f29143fc306203047fe2c9ec1554aa245fe9e
IgorSilvaZZ <igor06silva12@gmail.com>
2023-01-17 17:01:40 -0300

    Class one done

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,16 @@
+import { View, ActivityIndicator } from "react-native";
+
+export const Loading = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#09090A",
+      }}
+    >
+      <ActivityIndicator color='#7C3AEE' />
+    </View>
+  );
+};
