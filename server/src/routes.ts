15a832a7cc058157d674c79467aa67d9fad9837c
IgorSilvaZZ <igor06silva12@gmail.com>
2023-01-18 20:43:26 -0300

    Add toggle habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -43,6 +43,7 @@ export async function appRoutes(app: FastifyInstance) {
 
     const possibleHabits = await prisma.habit.findMany({
       where: {
+        // lte => Menor ou igual a data passada no body
         created_at: {
           lte: date,
         },
@@ -71,5 +72,53 @@ export async function appRoutes(app: FastifyInstance) {
       possibleHabits,
       completedHabits,
     };
+  });
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    const togglehabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id: habitId } = togglehabitParams.parse(request.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: habitId,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: habitId,
+        },
+      });
+    }
   });
 }
